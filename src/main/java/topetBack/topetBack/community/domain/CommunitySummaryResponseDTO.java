a495d5c9962f943d8b478d71772fea8c4a9d60c0
skay138 <nine1352@gmail.com>
2024-08-01 15:13:25 +0900

    FEAT: 댓글 페이징 적용

---
+++ src/main/java/topetBack/topetBack/community/domain/CommunitySummaryResponseDTO.java
@@ -0,0 +1,32 @@
+package topetBack.topetBack.community.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.*;
+import topetBack.topetBack.comment.domain.CommentResponseDTO;
+import topetBack.topetBack.file.domain.FileResponseDTO;
+import topetBack.topetBack.like.domain.Like;
+import topetBack.topetBack.like.domain.LikeResponseDTO;
+import topetBack.topetBack.member.domain.Member;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommunityResponseDTO {
+	private Long id;
+	private LocalDateTime createdTime;
+	private LocalDateTime updatedTime;
+	private String title;
+	private Member author;
+	private String content;
+	private String hashtag;
+	private String category;
+	private String animal;
+    private List<FileResponseDTO> images;
+    private List<CommentResponseDTO> comments;
+    private List<Like> likesList;
+
+}
