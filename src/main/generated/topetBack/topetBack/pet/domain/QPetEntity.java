c8748458a8b06da384d516e34aa8ff7fee0b3de6
parkjunhee0510 <junhee5143@naver.com>
2024-08-13 14:03:51 +0900

    Create : Qclass

---
+++ src/main/generated/topetBack/topetBack/pet/domain/QPetEntity.java
@@ -0,0 +1,59 @@
+package topetBack.topetBack.pet.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QPetEntity is a Querydsl query type for PetEntity
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QPetEntity extends EntityPathBase<PetEntity> {
+
+    private static final long serialVersionUID = -445351219L;
+
+    public static final QPetEntity petEntity = new QPetEntity("petEntity");
+
+    public final StringPath allergy = createString("allergy");
+
+    public final StringPath birth = createString("birth");
+
+    public final StringPath gender = createString("gender");
+
+    public final StringPath health = createString("health");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath kind = createString("kind");
+
+    public final StringPath name = createString("name");
+
+    public final StringPath neutered = createString("neutered");
+
+    public final StringPath profileSrc = createString("profileSrc");
+
+    public final StringPath type = createString("type");
+
+    public final StringPath uid = createString("uid");
+
+    public final StringPath weight = createString("weight");
+
+    public QPetEntity(String variable) {
+        super(PetEntity.class, forVariable(variable));
+    }
+
+    public QPetEntity(Path<? extends PetEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QPetEntity(PathMetadata metadata) {
+        super(PetEntity.class, metadata);
+    }
+
+}
+
---
+++ src/main/generated/topetBack/topetBack/shorts/domain/QShortsEntity.java
@@ -0,0 +1,59 @@
+package topetBack.topetBack.shorts.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QShortsEntity is a Querydsl query type for ShortsEntity
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QShortsEntity extends EntityPathBase<ShortsEntity> {
+
+    private static final long serialVersionUID = -916298093L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QShortsEntity shortsEntity = new QShortsEntity("shortsEntity");
+
+    public final topetBack.topetBack.member.domain.QMember author;
+
+    public final StringPath content = createString("content");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath thumbnailPhotoSrc = createString("thumbnailPhotoSrc");
+
+    public final StringPath title = createString("title");
+
+    public final StringPath videoSrc = createString("videoSrc");
+
+    public QShortsEntity(String variable) {
+        this(ShortsEntity.class, forVariable(variable), INITS);
+    }
+
+    public QShortsEntity(Path<? extends ShortsEntity> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QShortsEntity(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QShortsEntity(PathMetadata metadata, PathInits inits) {
+        this(ShortsEntity.class, metadata, inits);
+    }
+
+    public QShortsEntity(Class<? extends ShortsEntity> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.author = inits.isInitialized("author") ? new topetBack.topetBack.member.domain.QMember(forProperty("author")) : null;
+    }
+
+}
+
